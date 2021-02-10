171bbe9dda9c7d27addf37e7326216e9a87cef06
SergeyArenzon <sergeyr1991@gmail.com>
2021-02-10 23:35:34 +0200

    fix rechoose bag

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -130,6 +130,8 @@ export default function Game() {
         if (player[0] === -1 && checkClicked(x, y, whosTrun)) {
             console.log("first");
             setPlayer([x, y]);
+        } else if (player[0] != -1 && checkClicked(x, y, whosTrun)) {
+            setPlayer([x, y]);
         } else if (!checkClicked(x, y, enemy)) {
             if (greyTurn) {
                 gMoveHandler(x, y);
